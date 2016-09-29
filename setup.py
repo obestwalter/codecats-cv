706aed8a4971e23de1a84c6ccaf6512316a30783
Oliver Bestwalter <oliver.bestwalter@avira.com>
2016-09-29 20:54:15 +0200

    minimal installable version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='codecats-cv',
+    author='Codecats',
+    url='https://github.com/obestwalter/codecats-cv',
+    use_scm_version=True,
+    packages=['codecats_cv'],
+    license='Unlicense',
+)

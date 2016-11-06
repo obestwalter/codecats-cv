ebbd6f903ce515c743a01245b976569a5b828d48
Oliver Bestwalter <oliver.bestwalter@avira.com>
2016-11-06 21:25:13 +0100

    rudimentary login

---
+++ codecats_cv/authentication.py
@@ -0,0 +1,23 @@
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class User(UserMixin):
+    user_database = {"MaxMuster": ("MaxMuster", "Max")}
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls, id_):
+        return cls.user_database.get(id_)
+
+
+class LoginForm(FlaskForm):
+    password = StringField('password', validators=[DataRequired()])
+
+    def validate(self):
+        return self.password.data == 'rumpelstielzchen'
--- codecats_cv/controller.py
+++ codecats_cv/controller.py
@@ -1,8 +1,9 @@
 import logging
 
-from flask import Flask
-from flask.templating import render_template
+from flask import Flask, render_template, url_for, redirect, request
+from flask_login import LoginManager, login_required
 
+from codecats_cv.authentication import LoginForm, User
 from codecats_cv.cnf import PATH, STATIC, SECRETS, data
 from codecats_cv.model import get_experiences
 
@@ -13,16 +14,13 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = SECRETS.SECRET_KEY
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-
-# TODO use session and auth
-class User:
-    def __init__(self, name):
-        self.name = name
-
-    @property
-    def isLoggedIn(self):
-        return False
+@login_manager.user_loader
+def load_user(id_):
+    return User.get(id_)
 
 
 @app.context_processor
@@ -30,17 +28,30 @@ def inject_data_into_templates():
     return dict(STATIC=STATIC)
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # this is a form from Flask-WTF for example purposes,
+    # you can get the data any way you want
+    form = LoginForm()
+    if form.validate_on_submit():
+        # [load the user from the database]
+        # [check that their password is valid]
+        # login_user(user)
+        # flash("Logged in successfully.")
+        return redirect(request.args.get("next") or url_for("index"))
+
+    return render_template("login.html", form=form)
+
+
 @app.route('/')
 def index():
     return render_template(
-        'main.html',
-        MY=data.MY, experiences=get_experiences(), user=User('dohfie'))
+        'main.html', MY=data.MY, experiences=get_experiences())
 
 
 @app.route('/add-entry', methods=['GET', 'POST'])
+@login_required
 def add_entry():
-    # TODO use WTFORMS
-    # TODO check for post and add stuff from form
     return render_template('add-entry.html')
 
 
